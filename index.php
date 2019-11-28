c582a5bfee79dde6c771fd1fe8b381cfdbec4c4a
nghiapro123z <nghiatmgch17633@fpt.edu.vn>
2019-11-28 11:13:14 +0700

    ds

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+	<h1>Create Product</h1>
+	<from action="insertProduct.php" method="post">
+	Name <input type="text" name="name">
+	<br>
+	Price <input type="text" name="price">
+	<br>
+	<input type="submit" value="Insert">
+	</from>
+</body>
+</html>
